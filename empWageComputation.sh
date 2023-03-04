63f75faad70d2fe265ccd2510023f16dc91e6f76
“Viishah” <vtorsalle@gmail.com>
2023-03-04 10:36:14 +0530

    Add: Store the Day and the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,5 @@
 echo "welcome to Employee Wage Computation Program"
-isPartTime=1
+ isPartTime=1
 isFullTime=2
 empHrs=0
 totalSalary=0
@@ -7,6 +7,8 @@ totalWorkingHour=0;
 empRatePerHr=20
 numWorkingDays=20
 day=1;
+
+declare -A day
 
 function calculateWorkingHour(){
 case $randomCheck in $isFullTime)
@@ -21,24 +23,28 @@ esac
 
 while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
-randomCheck=$((RANDOM%3));
+	randomCheck=$((RANDOM%3));
 
-wHour=$(calculateWorkingHour $randomCheck)
-totalWorkingHour=$(($totalWorkingHour+$wHour))
+	wHour=$(calculateWorkingHour $randomCheck)
+	totalWorkingHour=$(($totalWorkingHour+$wHour))
 
 if [ $totalWorkingHour -gt 100 ]
 then
-totalWorkingHour=$(($totalWorkingHour - $wHour));
+	totalWorkingHour=$(($totalWorkingHour - $wHour));
 break
 fi
 salary=$(($empRatePerHr*$wHour))
 totalSalary=$(($totalSalary+$salary))
 ((day++))
+
 echo "Employee daily wage "${salary[@]}
 echo "Employee Total Wage "${totalSalary[*]}
+echo "Employee Day Wise Wage "${day[@]}
+
 done
 
 echo "Employee has earned $totalSalary $ in a month (Total Working Hour : $totalWorkingHour)"
+
 
 
 
